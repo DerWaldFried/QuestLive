5138e11d36c336483fc95dd6c75533e7b52be3cd
Patrick <patrick.b-privat@outlook.de>
2024-04-15 20:56:27 +0200

    Update QuestSystem.java
    Programming at the QuestSystem.
    
    -Added a Generator Class
    -Added some Methods

--- src/process/QuestSystem.java
+++ src/process/QuestSystem.java
@@ -1,12 +1,18 @@
 package process;
 
-public class QuestSystem {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class QuestSystem<Quest> {
 	private String name;
 	private String desc;
+	private boolean finished;
 	
-	public QuestSystem(String name, String desc) {
+	public QuestSystem(String name, String desc, boolean finished) {
 		this.name = name;
 		this.desc = desc;
+		this.finished = finished;
 	}
 	
 	public String getQName() {
@@ -14,5 +20,22 @@ public class QuestSystem {
 	}
 	public String getQDesc() {
 		return desc;
+	}
+	public boolean getQFinished() {
+		return finished;
+	}
+
+	class QuestGenerator{
+		private List<Quest> allQuest;
+		private Random random;
+		
+		public QuestGenerator() {
+			allQuest = new ArrayList<>();
+			random = new Random();
+			
+			//Erster Eintrag
+			allQuest.add((Quest) new QuestSystem<Quest>("Stay Fresh", "Putze dir deine Zähne", false));
+		}
+		
 	}
 }
