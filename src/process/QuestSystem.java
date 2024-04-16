35980b9d6fe8036c049fa0013cbdc6b216f791c1
Patrick <patrick.b-privat@outlook.de>
2024-04-16 08:05:23 +0200

    Update QuestSystem.java
    Deleted unused imports

--- src/process/QuestSystem.java
+++ src/process/QuestSystem.java
@@ -1,11 +1,5 @@
 package process;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Random;
-import java.util.Set;
-
 public class QuestSystem<Quest> {
 	private String name;
 	private String desc;
