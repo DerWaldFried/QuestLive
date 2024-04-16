3915b6634d9ebd453e4a821ca892c75488058c7e
Patrick <patrick.b-privat@outlook.de>
2024-04-16 09:57:45 +0200

    Update QuestSystem.java
    -Added on DoubleClick Method to open a Dialog, wenn User clicked 2times on Entry in List by QuestFrame

--- src/process/QuestSystem.java
+++ src/process/QuestSystem.java
@@ -1,5 +1,9 @@
 package process;
 
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
+
 public class QuestSystem<Quest> {
 	private String name;
 	private String desc;
@@ -30,4 +34,29 @@ public class QuestSystem<Quest> {
 	public String toString() {
 		return name+"→ "+desc+" XP: "+xp;
 	}
+
+	public void onDoubleClick() {
+
+		// Ich erstelle den Text für den Dialog
+        String message = "Name: " + getQName() + "\n" +
+                         "Beschreibung: " + getQDesc() + "\n" +
+                         "XP: " + getQXp();
+
+        // Erstelle einen Annehmen-Button und dazu passend den Action Listener
+        JButton acceptButton = new JButton("Annehmen");
+        acceptButton.addActionListener(e -> {
+            // Was passiert wenn Quest angenommen?
+            JOptionPane.showMessageDialog(null, "Quest angenommen: " + getQName());
+        });
+
+        // Nun Erstelle ich eine OptionPane mit dem Text und dem Annehmen-Button und binde ihn ein.
+        JOptionPane optionPane = new JOptionPane(message, JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new Object[]{acceptButton});
+        JDialog dialog = optionPane.createDialog("Quest Details");
+
+        // dialog sichtbar
+        dialog.setVisible(true);
+		
+	}
+	
+	
 }
