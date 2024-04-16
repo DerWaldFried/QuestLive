f112026aacdc3aa1c481288623df294c441da307
Friedmann <patrick.b-privat@outlook.de>
2024-04-16 20:55:23 +0200

    Create SettingsWindow.java
    Created the SettingsWindow
    
    Added Commentary
    
    Make first Example Entrys

---
+++ src/windows/SettingsWindow.java
@@ -0,0 +1,23 @@
+package windows;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JSlider;
+
+public class SettingsWindow {
+	public SettingsWindow() {
+        // Ich erstelle das Einstellungsfenster
+        JFrame settingsFrame = new JFrame("Einstellungen");
+        settingsFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        settingsFrame.setSize(300, 200);
+
+        // Ich Füge Einstellungsoptionen hinzu
+        JLabel difficultyLabel = new JLabel("Schwierigkeitsgrad:");
+        JSlider difficultySlider = new JSlider(1, 10, 5);
+        difficultyLabel.setLabelFor(difficultySlider);
+
+        settingsFrame.getContentPane().add(difficultyLabel);
+        settingsFrame.getContentPane().add(difficultySlider);
+        settingsFrame.setVisible(true);
+    }
+}
