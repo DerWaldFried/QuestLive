031eef87846399a1f0a5602eb40eb1e7defce70b
Patrick <patrick.b-privat@outlook.de>
2024-04-16 10:18:32 +0200

    Update QuestSystem.java
    Adding "accepted" Option to the Questsystem
    
    Added a Getter to become Information over a Quest ist accepted

--- src/process/QuestSystem.java
+++ src/process/QuestSystem.java
@@ -8,15 +8,18 @@ public class QuestSystem<Quest> {
 	private String name;
 	private String desc;
 	private int xp;
+	private boolean accepted;
 	private boolean finished;
 	
-	public QuestSystem(String name, String desc, int xp, boolean finished) {
+	public QuestSystem(String name, String desc, int xp, boolean accepted, boolean finished) {
 		this.name = name;
 		this.desc = desc;
 		this.xp = xp;
+		this.accepted = accepted;
 		this.finished = finished;
 	}
 	
+	//Getter dienen der späteren Funktionalitäten in den JDialog Bereichen
 	public String getQName() {
 		return name;
 	}
@@ -26,13 +29,20 @@ public class QuestSystem<Quest> {
 	public int getQXp() {
 		return xp;
 	}
+	public String getQAccepted() {
+		if(accepted == true) {
+			return "Ja";
+		}else {
+			return "Nein";
+		}
+	}
 	public boolean getQFinished() {
 		return finished;
 	}
 	
 	@Override
 	public String toString() {
-		return name+"→ "+desc+" XP: "+xp;
+		return name+"→ "+desc+" XP: "+xp+" | Angenommen: "+accepted;
 	}
 
 	public void onDoubleClick() {
@@ -47,6 +57,8 @@ public class QuestSystem<Quest> {
         acceptButton.addActionListener(e -> {
             // Was passiert wenn Quest angenommen?
             JOptionPane.showMessageDialog(null, "Quest angenommen: " + getQName());
+            accepted = true;
+            System.out.println("Quest angenommen?:"+getQAccepted());
         });
 
         // Nun Erstelle ich eine OptionPane mit dem Text und dem Annehmen-Button und binde ihn ein.
