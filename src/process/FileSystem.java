56608073fbc980d3d0dafd6c899f9b7f50cf68da
Patrick <patrick.b-privat@outlook.de>
2024-04-17 10:51:18 +0200

    Create FileSystem.java
    - Added Filesystem to check for a File and create a File
    
    - Will be updated with Time

---
+++ src/process/FileSystem.java
@@ -0,0 +1,39 @@
+package process;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileSystem {
+
+	String working_dir = System.getProperty("user.dir");
+	
+	public boolean Filechecker(File file) {
+		boolean checkup;
+		
+			File filePath = new File(working_dir + "/" + file);
+			if(filePath.exists()) {
+				checkup = true;
+			}else {
+				checkup = false;
+			}
+		
+		return checkup;
+	}
+	
+	private void createFileConfig(File file) {
+		boolean checkupCreate;
+		
+		try {
+			File configFileObj = new File(working_dir+file);
+		    if (configFileObj.createNewFile()) {
+		    	System.out.println("File created: " + configFileObj.getName());
+		    	} else {
+		    		System.out.println("File already exists.");
+		    	}
+		    } catch (IOException e) {
+		      System.out.println("An error occurred.");
+		      e.printStackTrace();
+		    }
+	}
+	
+}
