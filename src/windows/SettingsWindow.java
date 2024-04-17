4de558208bfa43ce7d763d72466eaa0a8e1b6045
Patrick <patrick.b-privat@outlook.de>
2024-04-17 09:01:51 +0200

    Update SettingsWindow.java
    Updated the Window with new Options to config:
    
    -Time to Wake up
    -Difficulty Level
    
    Added a
    -Save Button

--- src/windows/SettingsWindow.java
+++ src/windows/SettingsWindow.java
@@ -1,23 +1,55 @@
 package windows;
 
+import java.util.Calendar;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JSlider;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerDateModel;
 
 public class SettingsWindow {
     public SettingsWindow() {
         // Ich erstelle das Einstellungsfenster
         JFrame settingsFrame = new JFrame("Einstellungen");
         settingsFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        settingsFrame.setSize(300, 200);
+        settingsFrame.setSize(400, 200);
+        settingsFrame.getContentPane().setLayout(new BoxLayout(settingsFrame.getContentPane(),  BoxLayout.Y_AXIS));
 
-        // Füge Einstellungsoptionen hinzu
+        /*
+         * Komponenten der Einstellungen
+         */
+        
+        //Schwierigkeitsgrad Komponenten
         JLabel difficultyLabel = new JLabel("Schwierigkeitsgrad:");
         JSlider difficultySlider = new JSlider(1, 10, 5);
         difficultyLabel.setLabelFor(difficultySlider);
+        
+        //Aufsteh Timer Komponenten Text
+        JLabel standUpTimer = new JLabel("Wann müssen sie immer aufstehen?");
+        JLabel standUPMin = new JLabel("Achtung: Schlafenszeit setzt automatisch 8 Stunden.");
+        
+        //Aufsteh Timer Komponenten
+        SpinnerDateModel model = new SpinnerDateModel();
+        model.setCalendarField(Calendar.MINUTE);
+        JSpinner timeSpinner = new JSpinner(model);
+        JSpinner.DateEditor editor = new JSpinner.DateEditor(timeSpinner,"HH:mm");
+        timeSpinner.setEditor(editor);
+        standUpTimer.setLabelFor(timeSpinner);
+        
+        //Schaltfläche zum Speichern
+        JButton saveConfigBTN = new JButton("Speichern"); 
 
+        //Einbinden ins Fenster
         settingsFrame.getContentPane().add(difficultyLabel);
         settingsFrame.getContentPane().add(difficultySlider);
+        settingsFrame.getContentPane().add(standUpTimer);
+        settingsFrame.getContentPane().add(timeSpinner);
+        settingsFrame.getContentPane().add(standUPMin);
+        settingsFrame.add(saveConfigBTN);
+        settingsFrame.setLocationRelativeTo(null);
         settingsFrame.setVisible(true);
     }
 }
