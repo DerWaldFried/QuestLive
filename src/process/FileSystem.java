3cc492edb5a33ad80ae12c1c9984c9424fcdb3f1
Patrick <patrick.b-privat@outlook.de>
2024-04-17 12:52:28 +0200

    Update FileSystem.java
    - Error Handling
    
    -Added writeFile and loadConfig

--- src/process/FileSystem.java
+++ src/process/FileSystem.java
@@ -1,10 +1,19 @@
 package process;
 
+import java.beans.Expression;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import windows.SettingsWindow;
 
 public class FileSystem {
-
+	
 	String working_dir = System.getProperty("user.dir");
 	
 	public boolean Filechecker(File file) {
@@ -25,7 +34,7 @@ public class FileSystem {
 		boolean checkupCreate;
 		
 		try {
-			File configFileObj = new File(working_dir+file);
+			File configFileObj = new File(working_dir+"/"+file);
 		    if (configFileObj.createNewFile()) {
 		    	System.out.println("File created: " + configFileObj.getName());
 		    	} else {
@@ -35,6 +44,42 @@ public class FileSystem {
 		      System.out.println("An error occurred.");
 		      e.printStackTrace();
 		    }
+	}
+	
+	public void writeFile(File file, Object... values) {
+		
+		try {
+			FileWriter fwrite = new FileWriter(working_dir+"/"+file);
+			for (Object value : values) {
+                fwrite.write(value.toString() + "\n");
+                System.out.println(value);
+            }
+			fwrite.close();
+			
+			System.out.println("Schreiben in Datei ausgeführt.");
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void loadConfig(File file) {
+		try {
+            BufferedReader reader = new BufferedReader(new FileReader(working_dir+"/"+file));
+            String dateTimeStr = reader.readLine();
+            String diffStr = reader.readLine();
+            reader.close();
+
+            SimpleDateFormat formatter = new SimpleDateFormat("HH");
+            Date time = formatter.parse(dateTimeStr);
+            int difficulty = Integer.parseInt(diffStr);
+            
+        } catch (IOException e) {
+        	// Wenn das laden nicht funktioniert
+            e.printStackTrace();
+        } catch (ParseException e) {
+			// Wenn Parse nicht funktioniert
+			e.printStackTrace();
+		}
 	}
 	
 }
