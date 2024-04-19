d32e4bfb4729a2ec1f42750bba828d5d3ecc1769
Friedmann <patrick.b-privat@outlook.de>
2024-04-19 22:27:01 +0200

    Update QuestLoader.java
    - Set this File Off

--- src/pluginsystem/QuestLoader.java
+++ src/pluginsystem/QuestLoader.java
@@ -5,9 +5,12 @@ import java.util.List;
 import process.QuestSystem;
 import windows.SettingsWindow;
 
+/*
+ * Diese Datei ist unrelevant, sie wurde vorübergehend wegen nicht lösbaren Problemen auf Ebene vom Java Grundkurs1 deaktiviert
+ */
 public class QuestLoader {
 	public static List<QuestSystem> getNewQuests() {
-        SettingsWindow settingsWindow = new SettingsWindow();
-        return settingsWindow.getNewQuests();
+        return null;
+        //return SettingsWindow.getNewQuests();
     }
 }
